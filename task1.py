a7d4fae5d17c5c32cdec3c1736316e72b6a7b9dd
anyatronina <55392547+anyatronina@users.noreply.github.com>
2019-09-22 18:00:36 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 def parking(day, hour):
     b = day % 2
-    if 19 <= a <= 20:
+    if 19 <= hour <= 20:
         return 'both'
-    elif (b == 0 and 21 <= a <= 23) or (b == 1 and 18 >= a):
+    elif (b == 0 and 21 <= hour <= 23) or (b == 1 and 18 >= hour):
         return 'left'
-    elif (b == 0 and 18 >= a) or (b == 1 and 21 <= a <= 23):
+    elif (b == 0 and 18 >= hour) or (b == 1 and 21 <= hour <= 23):
         return 'right'
