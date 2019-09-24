4bc5dfc2eb1b973536f1ebe7e92a6f8388a7edc9
anyatronina <55392547+anyatronina@users.noreply.github.com>
2019-09-24 19:43:33 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,22 +1,14 @@
-# dist - расстояние от дома
-# way - весь пройденный путь
-
-n = 20
-
-
-def working(m):
-    dist = way = 0
-    i = 1
-    while i <= m:
-        way += 1 / i
-        if i % 2 == 1:
-            dist += 1 / i
-        else:
-            dist -= 1 / i
-        i += 1
-    dist = int(dist * 100000) / 100000
-    way = int(way * 100000) / 100000
-    return dist, way
-
-
-print(working(n))
+def palindrom(n):
+    k=0 #количество цифр в числе
+    m=n
+    while m>0: #подсчет цифр
+        k+=1
+        m=m//10
+    if k == 3:
+        if n%10 == 0 and n//10%10 == n//100%10:
+            return 'palindrom'
+        else: 'ne palindrom'
+    elif k == 4 and n//10%10 == n//100%10 and n%10 == n//1000:
+        return 'palindrom'
+    else:
+        return 'ne palindrom'
