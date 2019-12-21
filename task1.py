9b0c2654c991bd66ea500b8bca890cabd8080af2
anyatronina <55392547+anyatronina@users.noreply.github.com>
2019-12-21 16:36:24 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,8 +0,0 @@
-def parking(day, hour):
-    b = day % 2
-    if 19 <= hour <= 20:
-        return 'both'
-    elif (b == 0 and 21 <= hour <= 23) or (b == 1 and 18 >= hour):
-        return 'left'
-    elif (b == 0 and 18 >= hour) or (b == 1 and 21 <= hour <= 23):
-        return 'right'
